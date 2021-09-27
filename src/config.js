be184e5426ef6524250d43d23eb315628f22e3de
Kiven <abysus@163.com>
2021-09-27 17:26:46 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,57 @@
+const contractName = 'dev-1632583403511-11715691622706';
+
+module.exports = function getConfig() {
+  let config = {
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+    //walletUrl: "http://localhost:1234",
+    walletUrl: 'https://wallet.testnet.near.org',
+    helperUrl: "https://helper.testnet.near.org",
+    contractName,
+  };
+
+  if (process.env.REACT_APP_ENV !== undefined) {
+    config = {
+      ...config,
+      GAS: "300000000000000",
+      DEFAULT_NEW_ACCOUNT_AMOUNT: "5",
+      contractMethods: {
+        changeMethods: [
+          "new",
+          "mint_token",
+          "guest_mint",
+          "transfer",
+          "set_price",
+          "purchase",
+          "withdraw",
+          //nearDungeonHack Methods
+          "adventure",
+          "add_points",
+        ],
+        viewMethods: ["get_token_data", "get_num_tokens", "get_proceeds"],
+      },
+      accessKeyMethods: {
+        changeMethods: ["guest_mint", "set_price", "withdraw"],
+        viewMethods: [
+          "get_token_data",
+          "get_num_tokens",
+          "get_proceeds",
+          "get_pubkey_minted",
+        ],
+      },
+    };
+  }
+
+  if (process.env.REACT_APP_ENV === "prod") {
+    config = {
+      ...config,
+      networkId: "mainnet",
+      nodeUrl: "https://rpc.mainnet.near.org",
+      walletUrl: "https://wallet.near.org",
+      helperUrl: "https://helper.mainnet.near.org",
+      contractName: "near",
+    };
+  }
+
+  return config;
+};
